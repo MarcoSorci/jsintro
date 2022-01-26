b6aa3f8220ced9818ade5252df68008160dd864b
MarcoSorci <97511096+MarcoSorci@users.noreply.github.com>
2022-01-26 13:28:27 +0100

    crazy lists

--- linkedlist.js
+++ linkedlist.js
@@ -1,28 +1,48 @@
 
 
-let first = {value: 1, next: null, prev: null}
+let first = { value: 1, next: null, prev: null }
 
-let second = {value: 2, next: null, prev: first}
+let second = { value: 2, next: null, prev: first }
 
-first.next=second
+first.next = second
 
-let third = {value: 3, next: null, prev: second}
+let third = { value: 3, next: null, prev: second }
 
-second.next=third
+second.next = third
 
-let fourth = {value: 4, next: null, prev: third}
+let fourth = { value: 4, next: null, prev: third }
 
-third.next=fourth
+third.next = fourth
 
 let node = first;
 
-let mylinkedlist;
+let myfirstnode = {};
 
-function addelementtolinkedlist(linkedlist, value) {
-    if (linkedlist === undefined) {
+function addelementtolinkedlist(firstnode, value) {
+    if (firstnode.value === undefined) {
+        firstnode.value = value;
+        firstnode.next = null
+        firstnode.prev = null
+    } else {
         
+        let lastnode = firstnode;
+
+        while (lastnode.next !== null) {
+            lastnode = lastnode.next;
+        }
+
+        let node = {value: value, prev: lastnode, next: null}
+
+        lastnode.next = node;
     }
 }
+
+addelementtolinkedlist(myfirstnode, 6)
+console.log(myfirstnode);
+addelementtolinkedlist(myfirstnode, 16)
+console.log(myfirstnode);
+addelementtolinkedlist(myfirstnode, 116)
+console.log(myfirstnode);
 
 // while (true) {
 //     console.log(node.value);
@@ -40,7 +60,7 @@ while (node !== null) {
 
 let node2 = fourth                //backwards
 
-while (node2 !== null) {         
+while (node2 !== null) {
     console.log(node2.value);
     node2 = node2.prev;
 }
@@ -55,12 +75,12 @@ function linkedlisttoarray(list) {
 }
 console.log(linkedlisttoarray(first)); //[1,2,3,4]
 
-let array = [7,8,12,5]
+let array = [7, 8, 12, 5]
 
 
 
 function arraytolinkedlist(array) {
-    
+
 }
 
 console.log(arraytolinkedlist(array));
