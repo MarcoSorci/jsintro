f842058a181f77787f5ddf7353373b2684664592
MarcoSorci <97511096+MarcoSorci@users.noreply.github.com>
2022-02-01 21:51:02 +0100

    lost

--- consoleApp.js
+++ consoleApp.js
@@ -5,52 +5,69 @@
 //function that given word finds repetition count, and what index of each occurrence
 //button on the page, on the click gives prompt that returns how many times prompted word appears
 
-const text = "L'Ateniese Milziade, figlio di Cimone, spiccava fra tutti sia per l'antichità della stirpe, sia per la gloria degli antenati, sia per la propria saggezza ed aveva un'età tale che i suoi concittadini potevano non più solo concepire buone speranze su di lui, ma anche confidare che sarebbe stato quale ebbero poi modo, alla prova, di riscontrare, quando gli Ateniesi decisero di inviare dei coloni nel Chersoneso. Ce n'era un grande numero e molti chiedevano di partecipare alla spedizione, tra loro ne furono scelti alcuni e inviati a Delfi per consultare l'oracolo di Apollo su chi dovessero preferire come comandante. Quelle regioni infatti le occupavano allora i Traci e con loro bisognava combattere. La Pizia in risposta a chi la interrogava, ordinò espressamente che si prendessero come capo Milziade: se lo avessero fatto, l'impresa avrebbe avuto buon esito. In seguito al responso dell'oracolo, Milziade con truppe scelte parti con la flotta per il Chersoneso e, approdato a Lemno voleva ridurre gli abitanti dell'isola sotto il dominio degli Ateniesi e chiese ai Lemnii di arrendersi spontaneamente; quelli, schernendolo, risposero che lo avrebbero fatto quando lui, salpato con la flotta da casa sua, avesse raggiunto Lemno con il vento di tramontana. Questo vento infatti sorgendo da settentrione tiene la direzione contraria per chi parte da Atene. Milziade, non avendo tempo di trattenersi, indirizzò la rotta verso la sua meta e arrivò nel Chersoneso."
+let text = document.getElementById("text").textContent
 
-// function generatestats(string) {
-//     const charnumber = string.length
-//     const cleanarrayfromstring = cleanstringandconverttoarray(string)
-//     const wordnumber = cleanarrayfromstring.length
-//     const wordoccurrency = generateoccurrencycount(cleanarrayfromstring)
-//     const charoccurrency = generateoccurrencycount([...string])
-// }
+// function generateStats(string) {
 
-// function cleanstringandconverttoarray(string) {
-//     const cleanedstring = string.replace(/'/g, " ").replace(/,/g, "").replace(/\./g, "").replace(/;/g, "").replace(/:/g, "")
-//     const stringarray = cleanedstring.split(" ")
-//     return stringarray
-//}
+//     const charNumber = string.length;
+//     const cleanArrayFromString = cleanStringAndConvertInArray(string)
+//     const wordNumber = cleanArrayFromString.length;
+//     const wordOccurrency = generateOccurrencyCount(cleanArrayFromString);
+//     const charOccurrency = generateOccurrencyCount([...string]);
 
-// console.log("The char count is: " + charnumber);
-// for (const key in charoccurency) {
-//     if (Object.hasOwnProperty.call(charoccurency, key)) {
-//         const count = charoccurency[key];
-//         console.log("the char " + key + " appears " + count + (count === 1 ? " time" : " times"));
-
-//     }
-// }
-// console.log("-----------------------------------");
-// console.log("The word count is: " + wordnumber);
-// for (const key in wordoccurency) {
-//     if (Object.hasOwnProperty.call(wordoccurency, key)) {
-//         const count = wordoccurency[key];
-//         console.log("the word " + key + " appears " + count + (count === 1 ? " time" : " times"));
-
-//     }
-// }
-
-// function generateoccurrencycount(stringarray) {
-//     const occurrencycount = {}
-//     for (const word of stringarray) {
-//         if (occurrencycount[word]) {
-//             occurrencycount[word]++
-//         } else {
-//             occurrencycount[word] = 1
+//     console.log("Il numero dei caratteri è: " + charNumber);
+//     for (const key in charOccurrency) {
+//         if (Object.hasOwnProperty.call(charOccurrency, key)) {
+//             const count = charOccurrency[key];
+//             console.log("il carattere " + key + " compare: " + count + (count === 1 ? " volta" : " volte"));
 //         }
-//     } return occurrencycount
+//     }
+//     console.log("---------------------------------------------------")
+//     console.log("Il numero delle parole è: " + wordNumber);
+//     for (const key in wordOccurrency) {
+//         if (Object.hasOwnProperty.call(wordOccurrency, key)) {
+//             const count = wordOccurrency[key];
+//             console.log("la parola " + key + " compare: " + count + (count === 1 ? " volta" : " volte"));
+//         }
+//     }
 // }
 
-// generatestats(text)
+
+// function cleanStringAndConvertInArray(string) {
+
+//     const cleanedString = string.replace(/'/g, " ")
+//         .replace(/,/g, "")
+//         .replace(/;/g, "")
+//         .replace(/\./g, "")
+//         .replace(/:/g, "")
+//     const stringArray = cleanedString.split(" ");
+//     return stringArray;
+// }
+
+// function replaceAll(str, find, replace) {
+//     return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+// }
+
+
+
+// function generateOccurrencyCount(stringArray) {
+
+//     const occurrencyCount = {};
+
+//     for (const word of stringArray) {
+//         if (occurrencyCount[word]) {
+//             occurrencyCount[word]++;
+//         } else {
+//             occurrencyCount[word] = 1;
+//         }
+//     }
+
+//     return occurrencyCount;
+// }
+
+
+// generateStats(text);
+
 
 ////////////////////////////////////////////
 
@@ -82,6 +99,9 @@ function countachar(text, char) {
     for (let i = 0; i < temparray.length; i++) {
         if (temparray[i] === char.toLowerCase()) {
             counter++
+        }
+        if (char === " ") {
+            return "Space appears " + counter + " times"
         }
     } return counter
 }
@@ -121,7 +141,16 @@ function countawordandindex(array, word) {
 
 console.log(countawordandindex(textsplit, "milziade"));
 
-function promptMe() {
+function startSearch() {
     let useradjective = prompt("Enter word to search")
     alert(countawordandindex(textsplit, useradjective))
+}
+
+document.getElementById("stats").innerText = stats;
+function refresh() {
+    text = document.getElementById("text").textContent;
+    stats = "Char count: " + text.length + "\nWord count: " + (countaword(textsplit))
+        + "\n\nWord repetition:\n" + countawordandindex(textsplit)
+        + "\nChar repetition:\n" + countachar(textsplit);
+    document.getElementById("stats").innerText = stats;
 }
