580c52b9f90a9fe21d24d6384145209544acf45e
MarcoSorci <97511096+MarcoSorci@users.noreply.github.com>
2022-02-15 10:03:36 +0100

    Update what.js

--- what.js
+++ what.js
@@ -2,19 +2,18 @@ const scores = ["3:1", "2:2", "0:1", "4:2", "1:4", "1:1", "3:2", "1:2", "3:4", "
 
 function points(games) {
     let newarray = []
-    for (let i = 0; i < games.length; i++) {
-        const element = games[i];
-        let x = element[0]
-        let y = element[2]
-        if (x>y) {
+    for (const game of games) {
+        let x = game[0]
+        let y = game[2]
+        if (x > y) {
             newarray.push(3)
-        } else if (x<y) {
+        } else if (x > y) {
             newarray.push(0)
-        } else if (x=y) {
+        } else {
             newarray.push(1)
         }
-    } return newarray.reduce((p,c)=>p+c)
-        
+    } return newarray.reduce((p, c) => p + c)
+
 }
 
 console.log(points(scores));
